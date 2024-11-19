c69534e1cbbb7768276c11114c2515158cc88426
skttong <sommanuttong@gmail.com>
2024-11-19 09:24:21 +0700

    update

--- testreact/backend/server.js
+++ testreact/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 5000;
+const PORT = 6000;
 
 // Connect to MongoDB
 const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
